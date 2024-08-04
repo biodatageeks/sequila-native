4e71a44f8f8739133caec851440332cf23837f0d
Pavel Khamutou <p.khamutou@gmail.com>
2024-08-04 15:22:24 +0200

    reformat Cargo.toml and update cli tests

--- sequila/sequila-cli/src/main.rs
+++ sequila/sequila-cli/src/main.rs
@@ -1,8 +1,7 @@
 use datafusion::common::DataFusionError;
 use datafusion::config::ConfigOptions;
 use datafusion::error::Result;
-use datafusion::execution::context::SessionContext;
-use datafusion::prelude::*;
+use datafusion::prelude::{SessionConfig, SessionContext};
 use datafusion_cli::exec;
 use datafusion_cli::print_options::PrintOptions;
 use log::info;
@@ -37,14 +36,42 @@ async fn test_interval_rule_eq() -> datafusion::error::Result<()> {
     let config = SessionConfig::from(options);
     let ctx = SessionContext::new_with_sequila(config);
     let sql = "CREATE TABLE target (contig TEXT, pos_start INT, pos_end INT)";
-    ctx.sql(sql).await.unwrap();
+    ctx.sql(sql).await?;
     let sql = "CREATE TABLE read (contig TEXT, pos_start INT, pos_end INT)";
-    ctx.sql(sql).await.unwrap();
+    ctx.sql(sql).await?;
+
+    // from HashJoinExec
     let df = ctx
         .sql(
             "SELECT COUNT(*) FROM target a JOIN read b ON a.contig = b.contig AND a.pos_end >= b.pos_start AND a.pos_start <= b.pos_end",
         )
         .await?;
-    df.show().await?;
+
+    let plan = df.explain(false, false)?.collect().await?;
+    let formatted = datafusion::arrow::util::pretty::pretty_format_batches(&plan)?.to_string();
+    assert!(formatted.contains("IntervalJoinExec: "));
+
+    // from NestedLoopJoinExec
+    let df = ctx
+        .sql(
+            "SELECT COUNT(*) FROM target a JOIN read b ON a.pos_end >= b.pos_start AND a.pos_start <= b.pos_end",
+        )
+        .await?;
+
+    let plan = df.explain(false, false)?.collect().await?;
+    let formatted = datafusion::arrow::util::pretty::pretty_format_batches(&plan)?.to_string();
+    assert!(formatted.contains("IntervalJoinExec: "));
+
+    // cannot be transformed
+    let df = ctx
+        .sql(
+            "SELECT COUNT(*) FROM target a JOIN read b ON a.pos_end > b.pos_start AND a.pos_start <= b.pos_end",
+        )
+        .await?;
+
+    let plan = df.explain(false, false)?.collect().await?;
+    let formatted = datafusion::arrow::util::pretty::pretty_format_batches(&plan)?.to_string();
+    assert!(!formatted.contains("IntervalJoinExec: "));
+
     Ok(())
 }
