5910c186d295ba71617de67bac62eefbfccde607
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2024-11-10 11:36:02 +0100

    Making output less verbose by default (#30)
    * Making output less verbose by default
    
    * Changes to ms not ns

--- sequila/sequila-core/src/physical_planner/joins/interval_join.rs
+++ sequila/sequila-core/src/physical_planner/joins/interval_join.rs
@@ -37,11 +37,11 @@ use fnv::FnvHashMap;
 use futures::{ready, Stream, StreamExt, TryStreamExt};
 use std::any::Any;
 use std::collections::HashMap;
-use std::fmt;
 use std::fmt::{Debug, Formatter};
 use std::mem::size_of;
 use std::sync::Arc;
 use std::task::Poll;
+use std::{fmt, usize};
 type Position = usize;
 
 #[derive(Debug)]
@@ -923,10 +923,10 @@ impl IntervalJoinStream {
                     handle_state!(self.process_probe_batch())
                 }
                 IntervalJoinStreamState::ExhaustedProbeSide => {
-                    log::info!("{:?} finished execution, total processed batches: {:?}, total join time: {:?} nanos",
+                    log::info!("{:?} finished execution, total processed batches: {:?}, total join time: {:?} ms",
                         std::thread::current().id(),
                         self.join_metrics.output_batches.value(),
-                        self.join_metrics.join_time.value()
+                        self.join_metrics.join_time.value()/usize::pow(10, 6)
                     );
 
                     Poll::Ready(None)
@@ -945,11 +945,11 @@ impl IntervalJoinStream {
         build_timer.done();
 
         log::info!(
-            "{:?} is done building a hash table from {:?} batches, {:?} rows, took {:?} nanos",
+            "{:?} is done building a hash table from {:?} batches, {:?} rows, took {:?} ms",
             std::thread::current().id(),
             self.join_metrics.build_input_batches.value(),
             self.join_metrics.build_input_rows.value(),
-            self.join_metrics.build_time.value()
+            self.join_metrics.build_time.value() / usize::pow(10, 6)
         );
 
         self.state = IntervalJoinStreamState::FetchProbeBatch;
@@ -982,7 +982,7 @@ impl IntervalJoinStream {
                 self.join_metrics.input_batches.add(1);
                 self.join_metrics.input_rows.add(batch.num_rows());
 
-                log::info!(
+                log::debug!(
                     "{:?} is done reading batch {:?}",
                     std::thread::current().id(),
                     self.join_metrics.input_batches.value()
@@ -1042,7 +1042,7 @@ impl IntervalJoinStream {
         self.join_metrics.output_rows.add(result.num_rows());
         timer.done();
 
-        log::info!(
+        log::debug!(
             "{:?} is done processing batch {:?} with {:?} output rows",
             std::thread::current().id(),
             self.join_metrics.output_batches.value(),
