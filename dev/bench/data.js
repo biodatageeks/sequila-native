037b01581d51695728eac42d39eb427087349b5c
github-action-benchmark <github@users.noreply.github.com>
2024-11-16 22:29:42 +0000

    add Run SeQuiLa-native benchmark (cargo) benchmark result for f82bafb78925cfd90e14ff09bcb90467c8e76753

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731786956604,
+  "lastUpdate": 1731796182285,
   "repoUrl": "https://github.com/biodatageeks/sequila-native",
   "entries": {
     "Run SeQuiLa-native benchmark": [
@@ -408,6 +408,440 @@ window.BENCHMARK_DATA = {
           {
             "name": "Coitrees-chainOrnAna1-chainVicPac2-6-8",
             "value": 410202688096,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "biodatageeks",
+            "username": "biodatageeks"
+          },
+          "committer": {
+            "name": "biodatageeks",
+            "username": "biodatageeks"
+          },
+          "id": "f82bafb78925cfd90e14ff09bcb90467c8e76753",
+          "message": "Draft benchmark",
+          "timestamp": "2024-11-16T19:22:18Z",
+          "url": "https://github.com/biodatageeks/sequila-native/pull/39/commits/f82bafb78925cfd90e14ff09bcb90467c8e76753"
+        },
+        "date": 1731796180517,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Coitrees-fBrain-DS14718-exons-1-2",
+            "value": 82558087,
+            "range": "± 18091",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-fBrain-DS14718-exons-1-2",
+            "value": 174142718,
+            "range": "± 6432861",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-fBrain-DS14718-exons-1-2",
+            "value": 131324632,
+            "range": "± 4834165",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-fBrain-DS14718-exons-1-2",
+            "value": 173439214,
+            "range": "± 5632810",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-fBrain-DS14718-chainOrnAna1-1-6",
+            "value": 304267795,
+            "range": "± 6179266",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-fBrain-DS14718-chainOrnAna1-1-6",
+            "value": 556796095,
+            "range": "± 9814425",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-fBrain-DS14718-chainOrnAna1-1-6",
+            "value": 487852379,
+            "range": "± 27589501",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-fBrain-DS14718-chainOrnAna1-1-6",
+            "value": 587635375,
+            "range": "± 4530196",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-fBrain-DS14718-chainVicPac2-1-8",
+            "value": 979558671,
+            "range": "± 42199587",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-fBrain-DS14718-chainVicPac2-1-8",
+            "value": 1799438610,
+            "range": "± 23159975",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-fBrain-DS14718-chainVicPac2-1-8",
+            "value": 1682143638,
+            "range": "± 43488137",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-fBrain-DS14718-chainVicPac2-1-8",
+            "value": 1969436665,
+            "range": "± 63329455",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-fBrain-DS14718-chainXenTro3Link-1-7",
+            "value": 6015288031,
+            "range": "± 357975326",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-fBrain-DS14718-chainXenTro3Link-1-7",
+            "value": 11099957269,
+            "range": "± 279483000",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-fBrain-DS14718-chainXenTro3Link-1-7",
+            "value": 11122917463,
+            "range": "± 65856397",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-fBrain-DS14718-chainXenTro3Link-1-7",
+            "value": 12331560626,
+            "range": "± 80948785",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-fBrain-DS14718-chainMonDom5Link-1-7",
+            "value": 14592112063,
+            "range": "± 714316616",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-fBrain-DS14718-chainMonDom5Link-1-7",
+            "value": 27172008977,
+            "range": "± 517956",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-fBrain-DS14718-chainMonDom5Link-1-7",
+            "value": 26915298580,
+            "range": "± 107265378",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-fBrain-DS14718-chainMonDom5Link-1-7",
+            "value": 30702328170,
+            "range": "± 67545478",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-fBrain-DS14718-ex-anno-1-2",
+            "value": 170125374,
+            "range": "± 3734319",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-fBrain-DS14718-ex-anno-1-2",
+            "value": 343765662,
+            "range": "± 21125186",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-fBrain-DS14718-ex-anno-1-2",
+            "value": 301684557,
+            "range": "± 17262699",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-fBrain-DS14718-ex-anno-1-2",
+            "value": 371242971,
+            "range": "± 22826740",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-fBrain-DS14718-ex-rna-1-7",
+            "value": 2953613102,
+            "range": "± 90766651",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-fBrain-DS14718-ex-rna-1-7",
+            "value": 4960296457,
+            "range": "± 31276245",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-fBrain-DS14718-ex-rna-1-7",
+            "value": 3825079312,
+            "range": "± 21349819",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-fBrain-DS14718-ex-rna-1-7",
+            "value": 4075893252,
+            "range": "± 82104780",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-exons-fBrain-DS14718-2-1",
+            "value": 84278001,
+            "range": "± 1602927",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-exons-fBrain-DS14718-2-1",
+            "value": 166805607,
+            "range": "± 6708444",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-exons-fBrain-DS14718-2-1",
+            "value": 126107881,
+            "range": "± 1475583",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-exons-fBrain-DS14718-2-1",
+            "value": 167972946,
+            "range": "± 3762763",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-exons-chainOrnAna1-2-6",
+            "value": 434125458,
+            "range": "± 6666899",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-exons-chainOrnAna1-2-6",
+            "value": 802231537,
+            "range": "± 9104925",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-exons-chainOrnAna1-2-6",
+            "value": 662899170,
+            "range": "± 25706159",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-exons-chainOrnAna1-2-6",
+            "value": 793680487,
+            "range": "± 7884184",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-exons-chainVicPac2-2-8",
+            "value": 1392431751,
+            "range": "± 70172784",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-exons-chainVicPac2-2-8",
+            "value": 2279857045,
+            "range": "± 21450330",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-exons-chainVicPac2-2-8",
+            "value": 2208259828,
+            "range": "± 82983616",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-exons-chainVicPac2-2-8",
+            "value": 2491266572,
+            "range": "± 20148811",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-exons-chainXenTro3Link-2-7",
+            "value": 8108294127,
+            "range": "± 265590866",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-exons-chainXenTro3Link-2-7",
+            "value": 13283790082,
+            "range": "± 62242493",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-exons-chainXenTro3Link-2-7",
+            "value": 12941095425,
+            "range": "± 25420860",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-exons-chainXenTro3Link-2-7",
+            "value": 15407663173,
+            "range": "± 54326727",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-exons-chainMonDom5Link-2-7",
+            "value": 19723298235,
+            "range": "± 699883061",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-exons-chainMonDom5Link-2-7",
+            "value": 32592807753,
+            "range": "± 121980578",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-exons-chainMonDom5Link-2-7",
+            "value": 32083176718,
+            "range": "± 73832969",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-exons-chainMonDom5Link-2-7",
+            "value": 37291489269,
+            "range": "± 88459292",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-exons-ex-anno-2-2",
+            "value": 245118946,
+            "range": "± 13457897",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-exons-ex-anno-2-2",
+            "value": 443424050,
+            "range": "± 11213492",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-exons-ex-anno-2-2",
+            "value": 363667600,
+            "range": "± 8054144",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-exons-ex-anno-2-2",
+            "value": 477268001,
+            "range": "± 19006101",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-exons-ex-rna-2-7",
+            "value": 4564166750,
+            "range": "± 117480317",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-exons-ex-rna-2-7",
+            "value": 8009025150,
+            "range": "± 53412953",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-exons-ex-rna-2-7",
+            "value": 5485303433,
+            "range": "± 11268405",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-exons-ex-rna-2-7",
+            "value": 6031207402,
+            "range": "± 58241662",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-chainOrnAna1-fBrain-DS14718-6-1",
+            "value": 328554991,
+            "range": "± 24275395",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-chainOrnAna1-fBrain-DS14718-6-1",
+            "value": 536176179,
+            "range": "± 20677040",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-chainOrnAna1-fBrain-DS14718-6-1",
+            "value": 479914061,
+            "range": "± 7505021",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-chainOrnAna1-fBrain-DS14718-6-1",
+            "value": 559375182,
+            "range": "± 22895528",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-chainOrnAna1-exons-6-2",
+            "value": 436165176,
+            "range": "± 19268425",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-chainOrnAna1-exons-6-2",
+            "value": 729174016,
+            "range": "± 15781244",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-chainOrnAna1-exons-6-2",
+            "value": 628851435,
+            "range": "± 3883409",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-chainOrnAna1-exons-6-2",
+            "value": 810532536,
+            "range": "± 911731",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-chainOrnAna1-chainVicPac2-6-8",
+            "value": 383223195279,
+            "range": "± 11975080695",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "IntervalTree-chainOrnAna1-chainVicPac2-6-8",
+            "value": 420634436419,
+            "range": "± 1791182975",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ArrayIntervalTree-chainOrnAna1-chainVicPac2-6-8",
+            "value": 449974498771,
+            "range": "± 4534547474",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "AIList-chainOrnAna1-chainVicPac2-6-8",
+            "value": 430356638766,
+            "range": "± 1872477767",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Coitrees-chainOrnAna1-chainXenTro3Link-6-7",
+            "value": 1299137274847,
             "range": "± 0",
             "unit": "ns/iter"
           }
